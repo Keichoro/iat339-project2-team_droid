2942d5247bbf15dfcd4131875f31d430b0436400
kcngo <kcngo@sfu.ca>
2016-11-04 14:30:12 -0700

    Added calendars for both date of birth and meeting date in fill-out-form-template.html

--- Style Guide/js/fill-out-form.js
+++ Style Guide/js/fill-out-form.js
@@ -1,5 +1,17 @@
 $( function() {
     $( '#meeting-date' ).datepicker({
+      // yearRange: "-100:+0",
+      changeMonth: true,
+      changeYear: true
+    });
+});
+
+// <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+//   		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+$( function() {
+    $( '#dateofbirth' ).datepicker({
+      yearRange: "-100:+0",
       changeMonth: true,
       changeYear: true
     });
